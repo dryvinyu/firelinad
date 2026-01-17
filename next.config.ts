62c680c81d8c18c9fbd6dd3059f95e04c11907da
kizuner-bonely <1003306162@qq.com>
2026-01-17 14:37:32 +0800

    chore: configure env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    MONAD_RPC_URL: process.env.MONAD_RPC_URL,
+    MONAD_CHAIN_ID: process.env.MONAD_CHAIN_ID,
+    DEPLOYER_PRIVATE_KEY: process.env.DEPLOYER_PRIVATE_KEY,
+    SANDBOX_ADDRESS: process.env.SANDBOX_ADDRESS,
+    NEXT_PUBLIC_MONAD_RPC_URL: process.env.NEXT_PUBLIC_MONAD_RPC_URL,
+    NEXT_PUBLIC_MONAD_CHAIN_ID: process.env.NEXT_PUBLIC_MONAD_CHAIN_ID,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
