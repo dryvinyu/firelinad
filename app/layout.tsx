f5bb9feab586e2fd1cfc9f2b5b5b1f1bfbd853a3
kizuner-bonely <1003306162@qq.com>
2026-01-17 11:19:40 +0800

    feat: init

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+
+import { Toaster } from '@/components/ui/sonner'
+
+import './globals.css'
+
+const fira = localFont({
+  src: './fonts/FiraSans-Regular.ttf',
+  variable: '--font-fira',
+})
+
+export const metadata: Metadata = {
+  title: 'Monad Fireline',
+  description: 'Monad Fireline',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fira.variable} antialiased`}>
+        {children}
+        <Toaster richColors position="top-center" />
+      </body>
+    </html>
+  )
+}
